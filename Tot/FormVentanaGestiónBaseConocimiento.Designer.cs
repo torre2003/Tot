e746cda05f28a48cfc73969449e4d0405e96f515
torre2003 <torre2003@gmail.com>
2016-11-20 16:33:55 -0300

    a

--- Tot/FormVentanaGestiónBaseConocimiento.Designer.cs
+++ Tot/FormVentanaGestiónBaseConocimiento.Designer.cs
@@ -51,21 +51,19 @@
             this.tabControl_gestion_conocimiento.Controls.Add(this.tabPage_reglas);
             this.tabControl_gestion_conocimiento.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl_gestion_conocimiento.Location = new System.Drawing.Point(0, 0);
-            this.tabControl_gestion_conocimiento.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl_gestion_conocimiento.Name = "tabControl_gestion_conocimiento";
             this.tabControl_gestion_conocimiento.SelectedIndex = 0;
-            this.tabControl_gestion_conocimiento.Size = new System.Drawing.Size(1052, 826);
+            this.tabControl_gestion_conocimiento.Size = new System.Drawing.Size(789, 671);
             this.tabControl_gestion_conocimiento.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControl_gestion_conocimiento.TabIndex = 0;
             // 
             // tabPage_variables
             // 
             this.tabPage_variables.Controls.Add(this.control_gestion_variables);
-            this.tabPage_variables.Location = new System.Drawing.Point(4, 25);
-            this.tabPage_variables.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage_variables.Location = new System.Drawing.Point(4, 22);
             this.tabPage_variables.Name = "tabPage_variables";
-            this.tabPage_variables.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage_variables.Size = new System.Drawing.Size(1044, 797);
+            this.tabPage_variables.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage_variables.Size = new System.Drawing.Size(781, 645);
             this.tabPage_variables.TabIndex = 0;
             this.tabPage_variables.Text = "Variables";
             this.tabPage_variables.UseVisualStyleBackColor = true;
@@ -76,9 +74,9 @@
             this.control_gestion_variables.AutoSize = true;
             this.control_gestion_variables.Dock = System.Windows.Forms.DockStyle.Fill;
             this.control_gestion_variables.id = "";
-            this.control_gestion_variables.Location = new System.Drawing.Point(4, 4);
-            this.control_gestion_variables.Margin = new System.Windows.Forms.Padding(5);
-            this.control_gestion_variables.MinimumSize = new System.Drawing.Size(1040, 788);
+            this.control_gestion_variables.Location = new System.Drawing.Point(3, 3);
+            this.control_gestion_variables.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.control_gestion_variables.MinimumSize = new System.Drawing.Size(780, 640);
             this.control_gestion_variables.Name = "control_gestion_variables";
             this.control_gestion_variables.nombre = "";
             this.control_gestion_variables.rango_limitado = false;
@@ -86,7 +84,7 @@
             this.control_gestion_variables.rango_min = "";
             this.control_gestion_variables.ruta_archivo_imagen = "";
             this.control_gestion_variables.ruta_archivo_rtf = "";
-            this.control_gestion_variables.Size = new System.Drawing.Size(1040, 789);
+            this.control_gestion_variables.Size = new System.Drawing.Size(780, 640);
             this.control_gestion_variables.TabIndex = 0;
             this.control_gestion_variables.texto_consulta = "";
             this.control_gestion_variables.variable_de_inicio = false;
@@ -97,11 +95,10 @@
             this.tabPage_hechos.Controls.Add(this.button_update);
             this.tabPage_hechos.Controls.Add(this.label1);
             this.tabPage_hechos.Controls.Add(this.listBox_hechos);
-            this.tabPage_hechos.Location = new System.Drawing.Point(4, 25);
-            this.tabPage_hechos.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage_hechos.Location = new System.Drawing.Point(4, 22);
             this.tabPage_hechos.Name = "tabPage_hechos";
-            this.tabPage_hechos.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage_hechos.Size = new System.Drawing.Size(1044, 797);
+            this.tabPage_hechos.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage_hechos.Size = new System.Drawing.Size(781, 645);
             this.tabPage_hechos.TabIndex = 1;
             this.tabPage_hechos.Text = "Hechos";
             this.tabPage_hechos.UseVisualStyleBackColor = true;
@@ -110,9 +107,10 @@
             // button_update
             // 
             this.button_update.Image = global::Tot.Properties.Resources.update_256x256;
-            this.button_update.Location = new System.Drawing.Point(531, 173);
+            this.button_update.Location = new System.Drawing.Point(398, 141);
+            this.button_update.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button_update.Name = "button_update";
-            this.button_update.Size = new System.Drawing.Size(384, 374);
+            this.button_update.Size = new System.Drawing.Size(288, 304);
             this.button_update.TabIndex = 2;
             this.button_update.UseVisualStyleBackColor = true;
             this.button_update.Click += new System.EventHandler(this.button_update_Click);
@@ -120,29 +118,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 15);
+            this.label1.Location = new System.Drawing.Point(20, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Lista de hechos";
             // 
             // listBox_hechos
             // 
             this.listBox_hechos.FormattingEnabled = true;
-            this.listBox_hechos.ItemHeight = 16;
-            this.listBox_hechos.Location = new System.Drawing.Point(30, 35);
+            this.listBox_hechos.Location = new System.Drawing.Point(22, 28);
+            this.listBox_hechos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox_hechos.Name = "listBox_hechos";
-            this.listBox_hechos.Size = new System.Drawing.Size(375, 708);
+            this.listBox_hechos.Size = new System.Drawing.Size(282, 576);
             this.listBox_hechos.TabIndex = 0;
             // 
             // tabPage_reglas
             // 
             this.tabPage_reglas.Controls.Add(this.control_gestion_de_reglas);
-            this.tabPage_reglas.Location = new System.Drawing.Point(4, 25);
-            this.tabPage_reglas.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage_reglas.Location = new System.Drawing.Point(4, 22);
             this.tabPage_reglas.Name = "tabPage_reglas";
-            this.tabPage_reglas.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage_reglas.Size = new System.Drawing.Size(1044, 797);
+            this.tabPage_reglas.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage_reglas.Size = new System.Drawing.Size(781, 645);
             this.tabPage_reglas.TabIndex = 2;
             this.tabPage_reglas.Text = "Reglas";
             this.tabPage_reglas.UseVisualStyleBackColor = true;
@@ -154,31 +152,29 @@
             this.control_gestion_de_reglas.AutoSize = true;
             this.control_gestion_de_reglas.base_conocimiento = null;
             this.control_gestion_de_reglas.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.control_gestion_de_reglas.Location = new System.Drawing.Point(4, 4);
-            this.control_gestion_de_reglas.Margin = new System.Windows.Forms.Padding(5);
+            this.control_gestion_de_reglas.Location = new System.Drawing.Point(3, 3);
+            this.control_gestion_de_reglas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.control_gestion_de_reglas.Name = "control_gestion_de_reglas";
-            this.control_gestion_de_reglas.Size = new System.Drawing.Size(1036, 789);
+            this.control_gestion_de_reglas.Size = new System.Drawing.Size(775, 639);
             this.control_gestion_de_reglas.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(763, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(572, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(11, 10);
+            this.button1.Size = new System.Drawing.Size(8, 8);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // FormVentanaGestiónBaseConocimiento
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1052, 826);
+            this.ClientSize = new System.Drawing.Size(789, 671);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tabControl_gestion_conocimiento);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MinimumSize = new System.Drawing.Size(1067, 863);
+            this.MinimumSize = new System.Drawing.Size(804, 708);
             this.Name = "FormVentanaGestiónBaseConocimiento";
             this.Text = "Gestión Base de Conocimiento";
             this.tabControl_gestion_conocimiento.ResumeLayout(false);
